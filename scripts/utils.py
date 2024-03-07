c9348207e66813763bc5cd13ac5854f699f3337b
shubhu1026 <shubhu.p2000@gmail.com>
2024-03-06 21:49:58 -0500

    game loop, player, physics entities, tilemap, clouds

---
+++ scripts/utils.py
@@ -0,0 +1,16 @@
+import os
+
+import pygame
+
+BASE_IMAGE_PATH = 'data/images/'
+
+def load_image(path):
+    img = pygame.image.load(BASE_IMAGE_PATH + path).convert()
+    img.set_colorkey((0,0,0))
+    return img
+
+def load_images(path):
+    images = []
+    for img_name in sorted(os.listdir(BASE_IMAGE_PATH + path)):
+        images.append(load_image(path + '/' + img_name))
+    return images
